a599274b7df3197904569ac34c7e29bb715be325
Juanjo Arranz <juanjoarranzales@gmail.com>
2019-02-06 20:59:51 +0100

    Testing Reducers

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -10,7 +10,7 @@ export function createCourseSuccess( course ) {
     return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
-function updateCourseSuccess( course ) {
+export function updateCourseSuccess( course ) {
     return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
